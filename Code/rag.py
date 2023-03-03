2009cf4811a648aff556a32454e04bf8abe7af73
Linghesh17 <123230753+Linghesh17@users.noreply.github.com>
2023-03-04 01:30:37 +0530

    Update

--- Code/rag.py
+++ Code/rag.py
@@ -1,8 +1,8 @@
-import pickle
-from sklearn.preprocessing import LabelEncoder
+import numpy as np,pickle,pandas as pd
 from flask import Flask, render_template, request
 app = Flask(__name__)
 classifier=pickle.load(open("model.pkl",'rb'))
+vector=pickle.load(open("vector.pkl",'rb'))
 @app.route('/')
 @app.route('/register')
 def homepage():
@@ -12,15 +12,22 @@ def homepage():
 def register():
     if request.method == 'POST':
         l= request.form.get('name')
-        n = [l]
-        edata=LabelEncoder()
-        n=edata.fit_transform(n)
-        n=n.reshape(-1,1)
-        prediction=classifier.predict(n)
+        if(('Ma' in l)or('ma' in l)):
+            return render_template('ma invalid input.html')
+        # Ni and Da
+        rules=['Sa','Ri','Ga','Pa','Da','Ni',"Sa'","Ri'","Ga'","Pa'","Da'","Ni'","Sa.","Ri.","Ga.","Pa.","Ni.","Da.",'-']
+        l1=l.split(' ')
+        for i in l1:
+            if(i not in rules):
+                return render_template('invalid input.html')
+        l1=np.array([l])
+        l1 = vector.transform(l1)
+        n1=pd.DataFrame(l1.toarray(), columns=['da', 'ga', 'ni', 'pa', 'ri', 'sa'])
+        prediction=classifier.predict(n1)
         if(prediction==0):
-            return render_template('confirm.html',name=l,output='Predicted class:{}'.format('HAMSADVANI'))
+            return render_template('confirm.html',name=l,output='Predicted class:{}'.format('MOHANAM'))
         else:
-            return  render_template('confirm.html',name=l,output='Predicted class:{}'.format('MOHANAM'))
+            return  render_template('confirm.html',name=l,output='Predicted class:{}'.format('HAMSADVANI'))
 
 if __name__ == "__main__":
     app.run(debug=True)
